e1020585105339c306b5b4ce83d4ee6746fd1b64
Rajvickram <vikrampalani10@gmail.com>
2024-02-28 12:35:48 +0530

    Newly Added some Topics

---
+++ 2. Math-Function/math.js
@@ -0,0 +1,42 @@
+Math.round(5.8)
+Ans = 6
+
+
+Math.floor(5.8)
+Ans = 6
+
+Math.ceil(5.1), and (5.8)
+Ans = 6
+
+Math.abs(-9)
+Ans = 9
+
+Math.pow(8,5)
+Ans = 32768
+
+Math.min(8,5,7,3,5)
+Ans = 3
+
+
+Math.random()
+Ans = 0 - 1
+
+
+Math.random()*6
+Ans = 0 - 1
+
+Math.floor(Math.random()*6)+1
+Ans = 0 - 5
+
+
+Math.floor(Math.random()*100)+1
+Ans = 0 - 100
+
+Math.PI
+Ans = 3.141592653589793
+
+Math.E
+Ans = 2.718281828459045
+
+
+
---
+++ 3-String-in-Js/script.js
@@ -0,0 +1,30 @@
+let str1 = "Strive not to be a string"
+let str2 = "but rather to be a value"
+
+// console.log(str2.substring(4,12))
+
+// console.log(str1.toUpperCase("Strive","Aim"))
+
+let bill = "Rs.10"
+
+// console.log(bill.length)
+// console.log(bill.padEnd(7,0))
+// console.log(bill.padStart(2))
+
+// console.log(str1[8])
+
+// let my = " Raj vickram"
+
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+// console.log(name.trim())
+// console.log(name.replace("Raj","Aishu"))
+// console.log(name.random())
+// console.log(random(name))
+
+let name = "Raj vickram"
+let random = Math.floor(Math.random() * name.length)
+let letter = name[random]
+
+console.log(letter)
+console.log(name.length)
