1c51ed4db3e24681281147fdaf85ec0fdd62bb56
Rajvickram <vikrampalani10@gmail.com>
2024-02-29 13:23:02 +0530

    Modified

--- demo.js
+++ demo.js
@@ -1,21 +1,24 @@
 function call() {
-    var a,b,c,tot,avg
-    a = parseInt(document.getElementById("t1").value);
-    b = parseInt(document.getElementById("t2").value);
-    c = parseInt(document.getElementById("t3").value);
+    var a,b,c,tot,avg;
+    a = parseInt(document.getElementById("t1").value)
+    b = parseInt(document.getElementById("t2").value)
+    c = parseInt(document.getElementById("t3").value)
     tot = a + b + c;
     avg = tot / 3;
 
-    document.getElementById("t4").value = avg;
+    document.getElementById("t4").value = avg
 }
 
-var a,b,c,d,tot,avg
-a = parseInt(prompt("Enter a Number 1 :"))
-b = parseInt(prompt("Enter b Number 2 :"))
-c = parseInt(prompt("Enter c Number 3 :"))
-d = parseInt(prompt("Enter d Number 4 :"))
-tot = a + b * c + d;
-avg = tot / 4
-alert("Answer :" + avg)
 
-console.log("Answer :" + avg + "Average Value")
+var a,b,c,d,tot,average
+
+a = parseInt(prompt("Enter No 1 :"))
+b = parseInt(prompt("Enter No 2 :"))
+c = parseInt(prompt("Enter No 3 :"))
+d = parseInt(prompt("Enter No 4 :"))
+tot = a + b * c - d
+average = tot / 4
+
+alert("Ans :" + average)
+
+console.log("Finally Answer : " + average)
