31176c72be2a4a34d64db7301ff76a7143ea0100
Rajvickram <vikrampalani10@gmail.com>
2024-03-05 09:48:32 +0530

    Added

--- 3-String-in-Js/script.js
+++ 3-String-in-Js/script.js
@@ -1,30 +1,16 @@
 let str1 = "Strive not to be a string"
 let str2 = "but rather to be a value"
 
-// console.log(str2.substring(4,12))
+// Two Ways Add the String Character
 
-// console.log(str1.toUpperCase("Strive","Aim"))
+/*1 - Method */
 
-let bill = "Rs.10"
+console.log(str1+str2)
 
-// console.log(bill.length)
-// console.log(bill.padEnd(7,0))
-// console.log(bill.padStart(2))
+/*2 - Method */
 
-// console.log(str1[8])
+console.log(str1.concat(str2))
+ 
 
-// let my = " Raj vickram"
 
-// console.log(name.toUpperCase())
-// console.log(name.toLowerCase())
-// console.log(name.trim())
-// console.log(name.replace("Raj","Aishu"))
-// console.log(name.random())
-// console.log(random(name))
 
-let name = "Raj vickram"
-let random = Math.floor(Math.random() * name.length)
-let letter = name[random]
-
-console.log(letter)
-console.log(name.length)
--- demo.js
+++ demo.js
@@ -1,49 +1,24 @@
-// function call() {
-//     var a,b,c,tot,avg
-//     a = parseInt(document.getElementById("t1").value)
-//     b = parseInt(document.getElementById("t2").value)
-//     c = parseInt(document.getElementById("t3").value)
-//     tot = a + b + c
-//     avg = tot / 3
-
-//     document.getElementById("t4").value = avg
-// }
-
-
-// // var a,b,c,d,tot,average
-// // a = parseInt(prompt("No 1 :"))
-// // b = parseInt(prompt("No 2: "))
-// // c = parseInt(prompt("No 3: "))
-// // d = parseInt(prompt("No 4: "))
-// // tot = a + b + c * d
-// // average = tot /12
-
-
-// alert("Answer :" + average)
-// console.log("Answer for Average Value : " + average)
-
-
-
-
-// function press() {
-//     let len = document.getElementById("field").value
-
-//     let cm = len / 2.54
-
-//     let res = document.getElementById("result")
-//     res.innerHTML = cm.toFixed(2)+" " + "Inches"
-// }
-
-
-
 function set() {
-    let name = document.getElementById("given").value
+    let cm = Number(document.getElementById("t1").value)
 
-    let random = Math.floor(Math.random() * name.length)
+    let inch = cm / 2.54
 
-    let ans = document.getElementById("rest")
+    let res = document.getElementById("t2")
 
-    ans.innerHTML = name[random]
+    res.innerHTML = inch.toFixed(2) + " " + "Inches"
+}
 
-    console.log("Answer Random Name : " + ans)
+
+function change() {
+    let no = document.getElementById("field").value
+
+    let form = Math.floor(Math.random() * no.length)
+    
+    let rest = no[form]
+
+    let tel = document.getElementById("ans")
+
+    tel.innerHTML = rest + " " + "Numbers"
+
+    console.log("Random is : " + rest)
 }
