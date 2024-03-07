bbcb0d863a1dfdd3abf43f42c3450faf8c7f1354
Rajvickram <vikrampalani10@gmail.com>
2024-03-07 09:39:04 +0530

    modified

--- demo.js
+++ demo.js
@@ -8,3 +8,14 @@ function call() {
 
     document.getElementById("t4").value = avg.toFixed(2) + " " + "Values"
 }
+
+var a,b,c,total,average
+a = parseInt(prompt("Enter No 1 :"))
+b = parseInt(prompt("Enter No 2 :"))
+c = parseInt(prompt("Enter No 3 :"))
+total = a * b / c
+average = total * 12/100
+
+alert("Answer : " + average)
+
+console.log("Answer : " + average)
