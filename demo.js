32646312494a296e98a4d71deba1b55229f9090e
Rajvickram <vikrampalani10@gmail.com>
2024-03-19 09:52:36 +0530

    Modified

--- demo.js
+++ demo.js
@@ -4,49 +4,52 @@ function call() {
 
     b = parseInt(document.getElementById("t2").value)
 
-    c = parseInt(document.getElementById("t3").value)
+    c = parseInt(document.querySelector(".t3").value)
 
-    tot = a * b * c
+    tot = a + b + c
+    
+    avg = tot / 12 * 100 - 3
 
-    avg = tot / 3 * 100
-
-    document.querySelector(".ans").value = `${avg.toFixed(2)} values`
+    document.querySelector(".result").value = `${avg.toFixed(2)} values`
 }
 
-
-// Converting Centimeter to Length :
 
 
 function show() {
-    let ce = Number(document.querySelector(".input").value)
+    let ce = Number(document.querySelector(".input-field-1").value)
 
     let formula_1 = ce / 2.54
 
-    document.querySelector(".result_1").value = `${formula_1.toFixed(2)} values`
+    document.getElementById("result_1").value = `${formula_1.toFixed(2)} Inches`
 }
 
 
-// Random Letters Printing our name while Enter in Input box :
 
 
 function press() {
-    let name = document.querySelector(".input-field").value
+    let name = document.querySelector(".input-field-2").value
 
     let formula_2 = Math.floor(Math.random() * name.length)
 
-    let result = name[formula_2]
+    let rest = name[formula_2]
 
-    document.getElementById("result_2").value = `${result}`
+    document.getElementById("result_2").value = `${rest} words`
 }
 
 
-// Checking Total Length :
 
 
-function wordlength() {
-    let names = document.getElementById("name").value
+
+
+
+
+
+
+
+function take() {
+    let names = document.getElementById("input-field-3").value
 
     let formula_3 = names.length
 
-    document.querySelector(".result_3").value = `${formula_3} Length`
+    document.getElementById("para").innerHTML+= "<marquee>" + formula_3 + "</marquee>"
 }
