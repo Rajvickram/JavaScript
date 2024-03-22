438466909611957e104263968ff128223987cacd
Rajvickram <vikrampalani10@gmail.com>
2024-03-22 11:07:47 +0530

    Added

--- 7-Switch-in-Js/script.js
+++ 7-Switch-in-Js/script.js
@@ -1,102 +1,50 @@
-/* Chidambaram : 
-           Population = 56,41,81,893
-           Male LiteracyRate  = 95.57
-           Female LiteracyRate = 88.41
-           Total Literacy Rate = 91.99 
-           language = "Tamil"
-
-    Pondicherry : 
-           Population = 3,42,41,04,864
-           Male LiteracyRate  = 98.97
-           Female LiteracyRate = 90.41
-           Total LiteracyRate = 94.69
-           language = "Tamil, English, France"
-
-    Chennai : 
-           Population = 4,53,52,15,975
-           Male LiteracyRate  = 99.99 
-           Female LiteracyRate = 91.52
-           Total LiteracyRate = 95.70
-           language = "English, Malayalam"
-
-
-    Bengalore : 
-           Population = 4,52,51,14,864
-           Male LiteracyRate  = 99.00
-           Female LiteracyRate = 90.40
-           Total LiteracyRate = 94.20
-           language = "Kannada"
-
-
-           The Indian City Most Population had population of of which male and female were first Male is next Female and they language Spoken is 
-*/
-
-
-//  First Type of Program Without using Button (onclick() => Function) :
-
-
 let btn = document.querySelector("button")
 let resultdiv = document.createElement("div")
 resultdiv.id = 'result'
-
 document.querySelector(".container").appendChild(resultdiv)
 
-btn.addEventListener('click', displayStats)
 
-function displayStats() {
+
+btn.addEventListener('click', call)
+function call() {
     let inp = document.getElementById("input-range")
-
     let city = inp.options[inp.selectedIndex].value
 
-    console.log(city)
-
-    let Population = 0, Male_LiteracyRate = 0, Female_LiteracyRate = 0, Total_LiteracyRate = 0, Language = ''
+    let Population = 0, LiteracyRate = 0, Language = ''
 
     switch (city) {
         case 'Chidambaram' :
-            Population = 564181893
-            Male_LiteracyRate = 95.57
-            Female_LiteracyRate = 88.41
-            Total_LiteracyRate = 91.99
+            Population = 123456
+            LiteracyRate = 95.12
             Language = 'Tamil'
             break
 
-        case 'Pondicherry' :
-           Population = 3424104864
-           Male_LiteracyRate  = 98.97
-           Female_LiteracyRate = 90.41
-           Total_LiteracyRate = 94.69
-           language = "Tamil, English, France"
+        case 'Chennai' :
+            Population = 12365478
+            LiteracyRate = 98.12
+            Language = 'English'
             break
 
+        case 'Bangalore' :
+            Population = 2563214
+            LiteracyRate = 96.12
+            Language = 'Kannada'
+            break
 
-        case 'Chennai' :
-            Population = 4535215975
-           Male_LiteracyRate  = 99.99 
-           Female_LiteracyRate = 91.52
-           Total_LiteracyRate = 95.70
-           Language = "English, Malayalam"
-           break
+        case 'Mumbai' :
+            Population = 6541258
+            LiteracyRate = 97.12
+            Language = 'Hindi'
+            break
 
-        case 'Bengaluru' :
-           Population = 4525114864
-           Male_LiteracyRate  = 99.00
-           Female_LiteracyRate = 90.40
-           Total_LiteracyRate = 94.20
-           Language = "Kannada" 
-           break
+        case 'Delhi' :
+            Population = 985632
+            LiteracyRate = 97.85
+            Language = 'Marathi'
+            break
     }
 
+    let answer = `The Indian City of ${city} is most population level is ${Population} there are total Literacy Rate ${LiteracyRate} and they speak ${Language}`
 
-    let textvalue = `The Indian City Most Population ${city} had population of ${Population}of which ${Total_LiteracyRate} male and female were first Male is ${Male_LiteracyRate} next Female ${Female_LiteracyRate} and they language Spoken is ${Language}`
-
-    document.getElementById("result").innerHTML = textvalue
+    document.getElementById("result").innerHTML = answer
 }
-
-
-
-
-//  Second Type of Program using Button Type while => Onclick() : 
-
-//  Will shown in Next BS and JS Concept :
-
--- demo.js
+++ demo.js
@@ -7,18 +7,18 @@ function call() {
 
     c = parseInt(document.querySelector(".t3").value)
 
-    tot = a * b + c
+    tot = a + b + c
 
     avg = tot / 3 * 12 - 100
 
     document.getElementById("result_1").value = `${avg} values`
-
 }
 
 
 
+
 function show() {
-    let ce = Number(document.querySelector(".input-field").value)
+    let ce = document.querySelector(".input-field-1").value
 
     let formula_1 = ce / 2.54
 
@@ -27,63 +27,42 @@ function show() {
 
 
 
+
 function press() {
-    let name = document.querySelector(".input-field-1").value
+    let name = document.querySelector(".input-field-2").value
 
     let formula_2 = Math.floor(Math.random() * name.length)
 
     let result = name[formula_2]
 
-    document.getElementById("result_3").value = `${result} Words`
+    document.getElementById("result_3").value = `${result}`
 }
 
-
-
 function fdrate() {
-    let months = Number(document.querySelector(".input-field-2").value)
+    let months = Number(document.querySelector(".input-field-3").value)
 
     let rate = 0
 
     if (months <= 3) {
-        rate = 6.5
-    }
-
-    else if (months >=3 && months <= 6) {
         rate = 7
     }
 
+    else if (months >= 3 && months <= 6) {
+        rate = 8
+    }
+
     else if (months >= 7 && months <= 9) {
-        rate = 7.5
+        rate = 9
     }
 
     else if (months >= 10) {
-        rate = 8
+        rate = 10
     }
 
     document.getElementById("result_4").value = `${rate} %`
 }
 
 
-let dec = document.getElementById("decreaseBtn")
 
-let res = document.getElementById("resetBtn")
 
-let inc = document.getElementById("increaseBtn")
 
-let counts = document.getElementById("countlabel")
-
-let count = 0
-
-inc.onclick = function() {
-    count++
-    counts.textContent = count
-}
-
-res.onclick = function() {
-    count = 0
-    counts.textContent = count
-}
-dec.onclick = function() {
-    count--
-    counts.textContent = count
-}
