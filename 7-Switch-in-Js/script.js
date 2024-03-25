6fa0cd70246d8aba5e62323998914754259a4531
Rajvickram <vikrampalani10@gmail.com>
2024-03-25 10:46:50 +0530

    Modified in 7-Switch-in-Js File

--- 7-Switch-in-Js/script.js
+++ 7-Switch-in-Js/script.js
@@ -1,50 +1,53 @@
-let btn = document.querySelector("button")
-let resultdiv = document.createElement("div")
+let btn = document.querySelector('button')
+
+let resultdiv = document.createElement('div')
 resultdiv.id = 'result'
 document.querySelector(".container").appendChild(resultdiv)
 
 
 
-btn.addEventListener('click', call)
-function call() {
+btn.addEventListener('click', display)
+
+function display() {
     let inp = document.getElementById("input-range")
+
     let city = inp.options[inp.selectedIndex].value
 
     let Population = 0, LiteracyRate = 0, Language = ''
 
     switch (city) {
         case 'Chidambaram' :
-            Population = 123456
-            LiteracyRate = 95.12
+            Population = 985623
+            LiteracyRate = 85.10
             Language = 'Tamil'
             break
 
         case 'Chennai' :
-            Population = 12365478
-            LiteracyRate = 98.12
-            Language = 'English'
+            Population = 78546
+            LiteracyRate = 95.10
+            Language = 'English' 
             break
 
         case 'Bangalore' :
-            Population = 2563214
-            LiteracyRate = 96.12
+            Population = 215463 
+            LiteracyRate = 95
             Language = 'Kannada'
             break
 
         case 'Mumbai' :
-            Population = 6541258
-            LiteracyRate = 97.12
+            Population = 874523
+            LiteracyRate = 96.10
             Language = 'Hindi'
             break
 
         case 'Delhi' :
-            Population = 985632
-            LiteracyRate = 97.85
+            Population = 1236547 
+            LiteracyRate = 97.10
             Language = 'Marathi'
             break
     }
 
-    let answer = `The Indian City of ${city} is most population level is ${Population} there are total Literacy Rate ${LiteracyRate} and they speak ${Language}`
+    let text = `The Indian City is ${city} and the population is ${Population} the have Rate ${LiteracyRate} and the spoken lang is ${Language}`
 
-    document.getElementById("result").innerHTML = answer
+    document.getElementById('result').innerHTML = text
 }
