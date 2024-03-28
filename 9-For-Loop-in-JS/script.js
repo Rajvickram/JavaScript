d282fdf7c49853336ed9240974a841aedbd059c5
Rajvickram <vikrampalani10@gmail.com>
2024-03-28 11:14:05 +0530

    Added

--- 9-For-Loop-in-JS/script.js
+++ 9-For-Loop-in-JS/script.js
@@ -1,40 +1,70 @@
-//  In JavaScript in the same of an JAVA :
+// //  In JavaScript in the same of an JAVA :
 
-//  so we have a 3 - types of an Loop :
-//  (For loop, While Loop, Do-While Loop) :
+// //  so we have a 3 - types of an Loop :
+// //  (For loop, While Loop, Do-While Loop) :
 
-for (let i=1;i<=5;i++) {
-    console.log('hello')
-}
+// for (let i=1;i<=5;i++) {
+//     console.log('hello')
+// }
 
 
-//  Printing Reversed Order in (10 TO 1) :
+// //  Printing Reversed Order in (10 TO 1) :
+
+// for (let i=10;i>=1;i--) {
+//     console.log(i)
+// }
+
+
+// //  While Loop :
+// //  Same way of Reversed Order in (10 To 1) :
+
+
+// i = 10
+// while (i>=1) {
+//     console.log(i)
+//     i--;
+// }
+
+
+// //  Break Statement :
+
+// //  If the user a Enter a number means u can the stop the Prompt element or else If user enter a Name means the loop will not be ended and the Prompt also can never be closed :
+
+
+// while (true) {
+//     let num = Number(prompt('Enter a Number :'))
+
+//     if (!isNaN(num)) {
+//         break;
+//     }
+// }
+
+
+
+// Pratice Program 
+//  1. => Reversed Number While Using For Loop :
+
 
 for (let i=10;i>=1;i--) {
     console.log(i)
 }
 
 
-//  While Loop :
-//  Same way of Reversed Order in (10 To 1) :
+//  2 => Same Program  using While Loop :
 
+let i = 10
 
-i = 10
-while (i>=1) {
+while(i>=1) {
     console.log(i)
-    i--;
+    i--
 }
 
+//  3 => Get Value from Prompt the user will be Enter a Number only or else he will press any alphabet means won't get out that attribute :
 
-//  Break Statement :
-
-//  If the user a Enter a number means u can the stop the Prompt element or else If user enter a Name means the loop will not be ended and the Prompt also can never be closed :
-
-
-while (true) {
+while(true) {
     let num = Number(prompt('Enter a Number :'))
 
     if (!isNaN(num)) {
-        break;
+        break
     }
 }
